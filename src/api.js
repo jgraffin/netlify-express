3a7f5a2093cb58216a338f5510addace6c067f89
Julio Ferreira <julio.ferreira@gs.liferay.com>
2022-03-17 21:54:36 -0300

    Add headers to the request

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
 const serverless = require("serverless-http");
-
 const app = express();
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
   res.json([
     {
       id: "1",
